24c84babe63b7d3a4ecf94f1ae4edc95ec934f22
Christian Coenen <coenen.christian@outlook.com>
2020-08-14 19:17:02 +0200

    added simple instagram bot

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # PythonAutomation101
+
+- tested with python3.8
+
+- pip install selenium==3.141.0
+- pip install pyautogui==0.9.50
---
+++ instagram.py
@@ -0,0 +1,35 @@
+import time
+import pyautogui
+from selenium import webdriver
+
+# CONFIG - Replace with your data! #
+DRIVER_PATH = 'C:/Users/Nutzer/Desktop/chromedriver.exe'
+USERNAME = 'smarter.code.yt@gmail.com'
+PASSWORD = ''
+KEYWORD = '#python'
+
+driver = webdriver.Chrome(DRIVER_PATH)
+driver.implicitly_wait(5)
+driver.get('http://www.instagram.com/')
+driver.find_element_by_name('username').send_keys(USERNAME)
+driver.find_element_by_name('password').send_keys(PASSWORD)
+driver.find_element_by_xpath("//*[contains(text(), 'Log In')]").click()
+driver.find_element_by_xpath("//*[contains(text(), 'Not Now')]").click()
+driver.find_element_by_xpath("//*[contains(text(), 'Not Now')]").click()
+search_box = driver.find_element_by_xpath("//*[contains(text(), 'Search')]").click()
+# search_box.send_keys(KEYWORD) - Not working!
+# search_box.submit()
+pyautogui.write(KEYWORD)
+time.sleep(3)
+# For some reason we need two clicks (not working with one)
+pyautogui.press("enter")
+pyautogui.press("enter")
+time.sleep(5)
+# Extract all loaded posts, go through them and like
+posts = driver.find_elements_by_xpath('//a[contains(@href, "%s")]' % '/p/')
+for post in posts:
+    post.click()
+    post.find_element_by_xpath("//*[name()='svg' and @aria-label='Like']").click()
+    post.find_element_by_xpath("//*[name()='svg' and @aria-label='Close']").click()
+# If you want the window to close afterwards
+# driver.quit()

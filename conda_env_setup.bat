f8bf00cb91e90a8699a9595c38cf486f61715fb7
Christian Coenen <coenen.christian@outlook.com>
2020-08-15 15:21:28 +0200

    added auto env setup & fortnite bot

---
+++ conda_env_setup.bat
@@ -0,0 +1,10 @@
+setlocal
+set ENV_NAME="PythonAutomation101"
+call conda create -n %ENV_NAME% python=3.8 -y
+call conda activate %ENV_NAME%
+call pip install selenium==3.141.0
+call pip install pyautogui==0.9.50
+call pip install pynput==1.6.8
+call pip install psutil==5.7.2
+call conda install pywin32==227 -y
+endlocal
